5f3bbc625dd2beea6770bbda5c6056f2312ac14e
happy-bmo <duydl9803@gmail.com>
2024-10-18 10:33:28 +0700

    build sidebar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,12 +1,41 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeIconActive,
+    UserGroupIconActive,
+    LiveIconActive,
+} from '~/components/Icon';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h1>Sidebar</h1>
+            <Menu>
+                <MenuItem
+                    title="For your"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeIconActive />}
+                ></MenuItem>
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupIconActive />}
+                ></MenuItem>
+                <MenuItem
+                    title="LIVE"
+                    to={config.routes.live}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveIconActive />}
+                ></MenuItem>
+            </Menu>
         </aside>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
     return (
-        <div style={{ height: '1000px' }}>
+        <div>
             <h1>Home page</h1>
         </div>
     );
